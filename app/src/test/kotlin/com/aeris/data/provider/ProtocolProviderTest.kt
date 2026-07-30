eb41088791eef71c2860274cb3af655eff96e829
ShkodnikAI <shkodnik2@gmail.com>
2026-07-30 23:16:59 +0300

    test(phase2): update protocol count to 12

--- app/src/test/kotlin/com/aeris/data/provider/ProtocolProviderTest.kt
+++ app/src/test/kotlin/com/aeris/data/provider/ProtocolProviderTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class ProtocolProviderTest {
     @Test
     fun test_count_is_8() {
-        assertEquals(8, ProtocolProvider.allProtocols.size)
+        assertEquals(12, ProtocolProvider.allProtocols.size)
     }
 
     @Test
